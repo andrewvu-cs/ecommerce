cca37c6f5bfb01a5bf4fcf2fbf556713bf25f792
avu916 <andrewvu.cs@gmail.com>
2019-09-04 15:59:07 -0700

    added pictures to clothing categories

---
+++ src/pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import Directory from '../../components/Directory/Directory.component';
+
+import "./Homepage.styles.scss";
+
+
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
