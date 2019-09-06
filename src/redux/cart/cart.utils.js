53c1ebd988f06bb548e240d5779e5c757cbe96b5
avu916 <andrewvu.cs@gmail.com>
2019-09-06 16:35:21 -0700

    updated shop data with new data, updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,13 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+    const existingCartItem = cartItems.find(cartItem => cartItem.id === cartItemToAdd.id);
+
+    if (existingCartItem){
+        return cartItems.map(cartItem => 
+            cartItem.id === cartItemToAdd.id
+            ?{ ...cartItem, quantity: cartItem.quantity + 1}
+            : cartItem
+            )
+    }
+    // quantity prop gets attached the first time around since the if block won't run when it's a new item;
+    return [...cartItems, {...cartItemToAdd, quantity: 1}]
+}
