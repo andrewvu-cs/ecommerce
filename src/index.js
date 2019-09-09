943a7d23d633878a19219f4eb09cf0f7d8c5d30e
avu916 <andrewvu.cs@gmail.com>
2019-09-09 16:17:50 -0700

    provided local storage via redux-perist

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+import { store, persistor } from "./redux/store";
 
 import "./index.css";
 import App from "./App";
 
-import store from './redux/store';
-
 ReactDOM.render(
-    // to access the redux store, must wrap our entire app with Provider from react-redux pkg
-    // allows the store to be accessible through the app
+  // to access the redux store, must wrap our entire app with Provider from react-redux pkg
+  // allows the store to be accessible through the app
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,11 +1,24 @@
 // will be the base reducer object, combines all the other states
 
 import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
+// Accessing local storage
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
 
-export default combineReducers({
+// only need to whitelist cart bc user is being handled by firebase
+const persistConfig = {
+    key: 'root',
+    storage,
+    whitelist: ['cart']
+}
+
+const rootReducer = combineReducers({
     user: userReducer,
     cart: cartReducer
 });
+
+export default persistReducer(persistConfig, rootReducer)
