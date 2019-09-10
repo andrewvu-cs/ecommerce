7d67fdaa658fba3a36e6feab5dc4c781dfbe2fc8
avu916 <andrewvu.cs@gmail.com>
2019-09-09 17:02:53 -0700

    refactored collections from shop component its own respective component

--- src/pages/Shop/Shop.component.jsx
+++ src/pages/Shop/Shop.component.jsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from "../../components/CollectionPreview/CollectionPreview.component";
+import CollectionsOverview from '../../components/CollectionsOverview/CollectionsOverview.component';
 
-import {selectCollections} from '../../redux/shop/shop.selectors'
 const ShopPage = ({ collections }) => (
   <div className="shop-page">
-    {collections.map(({ id, ...collectionProps }) => (
-      <CollectionPreview key={id} {...collectionProps} />
-    ))}
+    <CollectionsOverview/>
   </div>
 );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-})
 
-export default connect(mapStateToProps)(ShopPage);
+
+export default ShopPage;
